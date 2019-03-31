e3a7d9ebc1941ee492229c7553230795e0fbf329
Mauro Bianchi <bianchimro@gmail.com>
2019-04-01 01:41:27 +0200

    artist detail

--- museum-frontend/src/components/TopBar.js
+++ museum-frontend/src/components/TopBar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 export default class TopBar extends React.Component {
 
     render (){
         return (
-            <nav className="navbar fixed-top navbar-light bg-light">
-                <a className="navbar-brand">Super Museo ITS</a>
+            <nav className="navbar fixed-top navbar-dark bg-primary">
+                <Link to="/" className="text-white navbar-brand">Museo ITS</Link>
             </nav>
         )
         
--- museum-frontend/src/pages/ArtistDetail.js
+++ museum-frontend/src/pages/ArtistDetail.js
@@ -1,11 +1,57 @@
 import React from 'react'
+import TopBar from '../components/TopBar'
 
 
 export default class ArtistDetail extends React.Component {
 
+    state = {
+        artist: null,
+    }
+
+    controller = new AbortController()
+
+    componentDidMount(){
+        console.log("the component was mounted")
+        const { match } = this.props
+        const { params } = match
+
+        this.request = fetch(`/api/artists/${params.id}/?format=json`, {signal: this.controller.signal})
+            .then(data => {
+                return data.json()
+                .then(jsonData => {
+                    this.setState({artist: jsonData})
+                    this.request = null
+                })
+            })
+    }
+
+    componentWillUnmount() {
+        console.log("will unmount")
+        if(this.request){
+            this.controller.abort();
+        }
+    }
+
     render(){
-        return (<div className="container-fluid">
-            hello there, artist detail!
+        const { artist } = this.state
+
+        return (<div className="container with-topbar ArtistDetail">
+            <TopBar></TopBar>
+            {artist && <div className="">
+                <div className="row">
+                    <div className="col col-3">
+                        <img className="artist" src={artist.image}></img>
+                    </div>
+                    <div className="col d-flex flex-column justify-content-center">
+                        <div className="p-2">
+                            <h2>{artist.name}</h2>
+                            <i>{artist.birth_date} - {artist.death_date}</i>
+                            <p className="mt-2">{artist.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>}
+
         </div>)
     }
 
