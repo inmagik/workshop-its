aca6ae7d1ba9678fa7efa4bdf407ff1633be06f7
Mauro Bianchi <bianchimro@gmail.com>
2019-04-01 01:25:47 +0200

    artists list

--- museum-frontend/src/pages/ArtistsList.js
+++ museum-frontend/src/pages/ArtistsList.js
@@ -2,6 +2,31 @@ import React from 'react'
 import TopBar from '../components/TopBar'
 import { Link } from 'react-router-dom'
 import chunk from 'lodash/chunk'
+import truncate from 'lodash/truncate'
+
+
+class ArtistCard extends React.Component {
+    render() {
+        const { artist } = this.props
+        return (
+            <div className="ArtistCard">
+                <div className="card">
+                    <img src={artist.image} className="card-img-top artist" alt={artist.name}/>
+                    <div className="card-body">
+                    <h5 className="card-title">{artist.name}</h5>
+                    <p className="card-text description">
+                        {truncate(artist.description, { length: 200 })}
+                    </p>
+                    <div className="text-center">
+                        <Link to={`/artists/${artist.id}`} className="btn btn-primary">Dettagli</Link>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
 
 
 export default class ArtistList extends React.Component {
@@ -35,20 +60,19 @@ export default class ArtistList extends React.Component {
 
     render(){
         const { artists } = this.state
-        const chunkedArtists = chunk(artists, 2)
+        const chunkedArtists = chunk(artists, 3)
 
         console.log("chunkedArtists", chunkedArtists)
 
         return (<div className="container-fluid pb-5 with-topbar">
             <TopBar></TopBar>
             {chunkedArtists.length > 0 && chunkedArtists.map((row, i) => (
-                <div className="row">
+                <div className="row mb-md-2" key={i}>
                     {row.map(artist => (
-                        <div className="col">
-                        {artist.name}
+                        <div className="col-md-4 mb-2" key={artist.id}>
+                            <ArtistCard artist={artist}></ArtistCard>
                         </div>
                     ))}
-
                 </div>
             ))}
 
